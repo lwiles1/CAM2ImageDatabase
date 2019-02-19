87afcdc5861cd50a6aa9d1eea67f61a169d16f76
JeroSik <yoon161@purdue.edu>
2019-02-19 14:30:05 -0500

    Fix error checking for POST

--- imageDBcli.py
+++ imageDBcli.py
@@ -12,6 +12,7 @@ def rest():
 # GET Request: results corresponding to given query
 @rest.command(context_settings={"ignore_unknown_options": True})
 @click.argument('features', nargs=-1)
+# TODO Get by image ID as well
 def get(features):
   """
   Search by Camera ID or by features. Returns results of given query.
@@ -21,10 +22,10 @@ def get(features):
 
 # POST Request: send folder and optional csv file
 @rest.command(context_settings={"ignore_unknown_options": True})
-@click.argument('folder', type=click.Path(exists=True,
+@click.argument('folder', required=True, type=click.Path(exists=True,
                                           dir_okay=True,
                                           allow_dash=True))
-@click.argument('csv', required=False, type=click.Path(exists=False))
+@click.argument('csv', required=False, type=click.Path(exists=True, dir_okay=False))
 def post(folder, csv):
   """
   This uploads a folder of images and possibly the csv.
@@ -32,11 +33,8 @@ def post(folder, csv):
   # Make sure folder and file are appropriate
   if not os.path.isdir(folder):
     raise(click.BadParameter("Image directory not found or not a directory.", param_hint=folder))
-  if csv != None:
-    if not os.path.isfile(csv):
-      raise(click.BadParameter("csv file not found or not a file.", param_hint=csv))
-    if not csv.endswith('.csv'):
-      raise(click.BadParameter("File is not a csv file."))
+  if csv != None and not csv.endswith('.csv'):
+    raise(click.BadParameter("File is not a csv file.", param_hint=csv))
 
   # TODO folder is the directory of images and csv is the csv file if it exists
   if csv != None:
